a298c2c288304ee70ba74a3e3653ec133cb5bd3f
Camilo Sierra <csierra74@unab.edu.co>
2025-02-27 12:43:55 -0500

    rutas basicas

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,13 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class,'index']);
+
+Route::get('/product', [HomeController::class,'index']);
+
+Route::get('/product/{name}', function($name){
+    return $name;
+});
+
+Route::get('/product/{name}/{categoria}', function($name,$category){
+    return "$name y categoria $category";
+});
